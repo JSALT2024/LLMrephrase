7f600b9200b93d82f5411b60d7f3f3a4e778fb25
Dominik Macháček <gldkslfmsd@gmail.com>
2024-06-28 19:40:52 +0200

    bugfix

--- data-normalization/normalize-train.sh
+++ data-normalization/normalize-train.sh
@@ -29,15 +29,15 @@ if false; then  # change to true to do it again
 fi
 
 
-if false; then
-#if true; then
+#if false; then
+if true; then
 	cut -f 7 train_to_truecase | ./our-normalize.sh | p3/bin/python3 apply_truecase.py | sed 's/ \././g' > train_to_truecase.norm.tc &
 	cut -f 7 train_not_to_truecase | ./our-normalize.sh > train_not_to_truecase.norm
 	wait
 fi
 
-paste <( cut -f 1-6 train_to_truecase ) train_to_truecase.norm.tc > ts.train_to_truecase.norm.tc
-paste <( cut -f 1-6 train_not_to_truecase ) train_to_truecase.norm.tc > ts.train_not_to_truecase.norm
+paste <( cut -f 1-6 train_to_truecase ) train_to_truecase.norm.tc | python3 fix-sent-caps.py > ts.train_to_truecase.norm.tc
+paste <( cut -f 1-6 train_not_to_truecase ) train_not_to_truecase.norm > ts.train_not_to_truecase.norm
 
 # create the final norm file
 cat ts.train_not_to_truecase.norm ts.train_to_truecase.norm.tc > train.filtered3.beg_dur_id_frames_fps_text.norm.tsv
