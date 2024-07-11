869c0891daba2ad035c913639a026a6812fd9b85
vjavorek <javorek.v@gmail.com>
2024-07-11 14:53:02 -0400

    move to galdor A40s

--- LLama3-70Brephrase.sh
+++ LLama3-70Brephrase.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=12:ngpus=4:mem=512gb:plzen=True
+#PBS -q gpu@pbs-m1.metacentrum.cz
+#PBS -l select=1:ncpus=12:ngpus=4:mem=128gb:cl_galdor=True
 #PBS -l walltime=2:00:00
 #PBS -N Llama3-70Brephrase
 module load mambaforge
-conda activate Llama_HF
+conda activate /auto/plzen1/home/javorek/.conda/envs/Llama_HF
 cd /auto/brno2/home/javorek
 python LLama3-70Brephrase.py
